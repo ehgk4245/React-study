77f52cb371626b0bbe35542d88f03ad9c5f70af2
ehgk4245 <ehgkrk14@naver.com>
2025-07-03 12:39:02 +0900

    할일 목록 앞에 체크박스 추가 후 상태저장 및 수정 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,50 @@
 import { useState } from 'react'
 
 function App() {
-    const [todos, setTodos] = useState(['할일1', '할일2', '할일3'])
+    const [todos, setTodos] = useState([
+        {
+            checked: false,
+            todo: '할일 1',
+        },
+        {
+            checked: false,
+            todo: '할일 2',
+        },
+    ])
 
     const handleSubmit = (event) => {
         event.preventDefault()
 
-        setTodos([event.target.todo.value, ...todos])
+        if (event.target.todo.value.length == 0) {
+            alert('값이 비었다.')
+            return
+        }
+
+        addTodo(event.target.todo.value)
+
+        event.target.todo.value = ''
+    }
+
+    const addTodo = (todo) => {
+        setTodos([
+            {
+                checked: false,
+                todo: todo,
+            },
+            ...todos,
+        ])
     }
 
     const removeTodo = (selectedIndex) => {
         const filterTodos = todos.filter((todo, index) => index != selectedIndex)
         setTodos(filterTodos)
+    }
+
+    const updateTodo = (selectedIndex) => {
+        const checkTodo = todos.map((todo, index) =>
+            index == selectedIndex ? { ...todo, checked: !todo.checked } : todo,
+        )
+        setTodos(checkTodo)
     }
 
     return (
@@ -23,7 +56,8 @@ function App() {
             <ul>
                 {todos.map((todo, index) => (
                     <li key={index}>
-                        {todo}
+                        <input type="checkbox" checked={todo.checked} onChange={() => updateTodo(index)} />
+                        {todo.todo}
                         <button className="border rounded p-2 m-2" onClick={() => removeTodo(index)}>
                             삭제
                         </button>
